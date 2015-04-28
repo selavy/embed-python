fb9f5aae5502c27fa25fe4e5b9612fdb98dfaa30
Peter Lesslie <pclesslie@gmail.com>
2015-04-28 17:59:13 -0500

    Begin adding python function for callback

--- simple-framework/quote_source.c
+++ simple-framework/quote_source.c
@@ -8,6 +8,10 @@ int QuoteSource_Initialize( struct QuoteSource* src ) {
 }
 
 int QuoteSource_Finalize( struct QuoteSource* src ) {
+    if ( src->quoteCB ) {
+        Py_DECREF( src->quoteCB );
+    }
+
     src->quoteCB = 0;
     return 0;
 }
@@ -18,8 +22,35 @@ int QuoteSource_HandleQuote( struct QuoteSource* src, struct Quote* quote ) {
     free( txt );
 
     if ( src->quoteCB ) {
+        //
+        // TODO get the quote data and put in a python dict
+        //
+
+        PyObject* args = PyTuple_New( 1 );
+        //
+        // TODO convert quote to python dict
+        //
+        PyObject* pyQuote;
+        PyTuple_SetItem( args, 0, pyQuote );
+        PyObject_CallObject( src->quoteCB, args );
+
+        Py_DECREF( args );
+        Py_DECREF( pyQuote );
         src->quoteCB( quote );
     }
+    return 0;
+}
+
+int QuoteSource_SetQuoteCB( struct QuoteSource* src, PyObject* quoteCB ) {
+    if ( !quoteCB || PyCallable_Check( quoteCB ) ) {
+        return 1;
+    }
+
+    if ( src->quoteCB ) {
+        Py_DECREF( src->quoteCB );
+    }
+
+    src->quoteCB = quoteCB;
     return 0;
 }
 
--- simple-framework/quote_source.h
+++ simple-framework/quote_source.h
@@ -2,13 +2,16 @@
 #define QUOTE_SOURCE__H_
 
 #include "quote.h"
+#include <Python/Python.h>
 
 struct QuoteSource {
-    void (*quoteCB)( struct Quote* quote );
+//    void (*quoteCB)( struct Quote* quote );
+    PyObject* quoteCB;
 };
 
 int QuoteSource_Initialize( struct QuoteSource* src );
 int QuoteSource_Finalize( struct QuoteSource* src );
 int QuoteSource_HandleQuote( struct QuoteSource* src, struct Quote* quote );
+int QuoteSource_SetQuoteCB( struct QuoteSource* src, PyObject* quoteCB );
 
 #endif // QUOTE_SOURCE__H_
