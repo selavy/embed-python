ffb5c93702d116540c14eaeff645c7e3bb66a3f1
Peter Lesslie <pclesslie@gmail.com>
2015-04-30 05:33:53 -0500

    Use values in quote object

--- simple-framework/quote_callback.py
+++ simple-framework/quote_callback.py
@@ -1,3 +1,10 @@
 def quoteCb( quote ):
     print 'Python Quote Callback!'
+    print 'Last Trade Price: $', quote["lt"]
+    print 'Bid: $', quote["bid"]
+    print 'Ask: $', quote["ask"]
+
+    print 'Resetting bid to $99.99'
+    quote["bid"] = 99.99
+    print 'Bid: $', quote["bid"]
     return
