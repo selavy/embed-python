c9cb063559c1cb50d062d1fc00f01bb8b11c586d
Peter Lesslie <pclesslie@gmail.com>
2015-05-02 18:20:07 -0500

    Remove dead code

--- multithreaded/main.c
+++ multithreaded/main.c
@@ -83,39 +83,5 @@ int main( int argc, char *argv[] ) {
     free( threads );
     free( args );
 
-/*
-
-    for ( i = 0; i < N; ++i ) {
-        arg[i] = i+1;
-    }
-
-    for ( i = 0; i < N; ++i ) {
-        if (0 != pthread_attr_init( &(attr[i]) ) ) {
-            perror( "pthread_attr_init" );
-            exit( 1 );
-        }
-    }
-
-    for ( i = 0; i < N; ++i ) {
-        if ( 0 != pthread_create( &(thread[i]), &(attr[i]), routine, (void*) &(arg[i]) ) ) {
-            perror( "pthread_create" );
-            exit( 1 );
-        }
-    }
-
-    for ( i = 0; i < N; ++i ) {
-        if ( 0 != pthread_attr_destroy( &(attr[i]) ) ) {
-            perror( "pthread_attr_destroy" );
-            exit( 1 );
-        }
-    }
-
-    for ( i = 0; i < N; ++i ) {
-        if ( 0 != pthread_join( thread[i], &res ) ) {
-            perror( "pthread_join" );
-            exit( 1 );
-        }
-    }
-*/
     return 0;
 }
