a39bfed0afe8590d8f5e80a9f1dd168497060f10
Peter Lesslie <pclesslie@gmail.com>
2015-05-02 17:23:10 -0500

    Basic multithreaded setup

--- multithreaded/main.c
+++ multithreaded/main.c
@@ -1,7 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+
+void *routine( void *arg ) {
+    int *n = (int*) arg;
+    printf("routine(%d)\n", *n);
+    pthread_exit( 0 );
+}
 
 int main( int argc, char *argv[] ) {
-    printf( "hello world\n" );
+    pthread_t thread1, thread2;
+    pthread_attr_t attr1, attr2;
+    int i, j;
+    void *res;
+
+    if (0 != pthread_attr_init(&attr1)) {
+        perror( "attr1" );
+        exit( 1 );
+    }
+    if (0 != pthread_attr_init(&attr2)) {
+        perror( "attr2" );
+        exit( 1 );
+    }
+
+    i = 1;
+    j = 2;
+    if ( 0 != pthread_create( &thread1, &attr1, routine, (void*) &i ) ) {
+        perror( "pthread_create( thread1 )" );
+        exit( 1 );
+    }
+
+    if ( 0 != pthread_create( &thread2, &attr2, routine, (void*) &j ) ) {
+        perror( "pthread_create( thread2 )" );
+        exit( 1 );
+    }
+
+    if ( 0 != pthread_attr_destroy( &attr1 ) ) {
+        perror( "pthread_attr_destroy( attr1 )" );
+        exit( 1 );
+    }
+
+    if ( 0 != pthread_attr_destroy( &attr2 ) ) {
+        perror( "pthread_attr_destroy( attr2 )" );
+        exit( 1 );
+    }
+
+    if ( 0 != pthread_join( thread1, &res ) ) {
+        perror( "pthread_join( thread1 )" );
+        exit( 1 );
+    }
+
+    if ( 0 != pthread_join( thread2, &res ) ) {
+        perror( "pthread_join( thread2 )" );
+        exit( 1 );
+    }
+
     return 0;
 }
